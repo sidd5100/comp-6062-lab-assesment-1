521a67c603f80b63f877a3390ec206e4bf66f6b0
Siddhant Mahindrakar <siddhant5100@gmail.com>
2025-02-13 19:50:27 -0500

    la1

---
+++ COMP6062SMlA1/scripts/app.js
@@ -0,0 +1,15 @@
+
+
+const studentFullName = "Siddhant Mahindrakar";
+const studentNumber = "1191977";
+//const concat = studentFullName
+
+const result = `${studentFullName} - ${studentNumber}`;
+
+console.log(result);
+
+let headerContent = document.querySelector(".h1");
+
+headerContent.innerHTML = result;
+
+h1.classlist.add("headingPrimary");
